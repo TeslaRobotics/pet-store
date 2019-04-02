15a4c35d04fd48c392e95de0c438e752eb71e9e9
Ricardo <ry.yrupailla@gmail.com>
2019-04-02 18:43:41 -0500

    add restriction to owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,10 @@
 class Owner < ApplicationRecord
+  validates :birthday, presence: true
+  validate :validate_age
+
+  def validate_age
+    if birthday > 18.years.ago 
+      errors.add(:birthday, 'You should be over 18 years old.')
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -13,3 +14,11 @@ Pet.create(name: 'Flipper', description: 'Big black dog', age: 3, owner: Owner.f
 Service.create(description: 'Nice massage', price: 30.0)
 
 Ticket.create(service: Service.first, pet: Pet.first)
+
+
+100.times do {
+  dog_name = Faker::Creature::Dog.name
+
+
+}
+
