2f2cfe160555a95ecdb7f4291caf074b7b51077c
Ricardo <ry.yrupailla@gmail.com>
2019-04-02 20:00:56 -0500

    Fix bugs in seeds.rb

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,5 @@
 class Owner < ApplicationRecord
+  has_many :pets
   validates :birthday, presence: true
   validate :validate_age
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,42 @@ require 'faker'
 
 #first entries
 
+
+
+services = ["Nice massage", "Shower", "Sterilization", "Vaccination"]
+services.map! do |service| 
+  {description: service, price: (rand(60.0) + 1)} 
+end
+
+Service.create(services)
+
+
+#first entries
 Owner.create(name: 'mr. Robot', birthday: DateTime.new(1993,10,29))
 Pet.create(name: 'Flipper', description: 'Big black dog', age: 3, owner: Owner.first)
-Service.create(description: 'Nice massage', price: 30.0)
-
 Ticket.create(service: Service.first, pet: Pet.first)
 
+10.times do 
+  owner = {
+    name: Faker::Name.name,
+    birthday: Faker::Date.birthday(10, 85)
+  }
 
-100.times do {
-  dog_name = Faker::Creature::Dog.name
+  temp_owner = Owner.create(owner)
 
+  pet = {
+    name: Faker::Creature::Dog.name,
+    description: Faker::Books::Lovecraft.sentences[0],
+    age: (rand(15) + 1),
+    owner: temp_owner
+  }
+  
+  
+  temp_pet = Pet.create(pet) 
 
-}
+  rand(10).times do 
+    temp_service = Service.limit(1).offset(rand(Service.count)).to_a[0]
+    Ticket.create(service: temp_service, pet: temp_pet)
+  end
+end
 
